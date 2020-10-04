65d7057085143c81320fd959936ce1a25ff234dd
l-o30 <71224933+l-o30@users.noreply.github.com>
2020-10-04 07:40:24 +0200

    Correction
    Correcting Spelling On Orders

--- orders.php
+++ orders.php
@@ -5,10 +5,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Place Your Oders</title>
+    <title>Place Your Orders</title>
     <link rel="stylesheet" href="Style.css">
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
 
 </head>
 <body>
